bbdacd33be4d1d3a11ace44877b4e32a92888289
Michele de Rosa <michele.derosa@yahoo.com>
2021-11-11 17:43:13 +0100

    responsive navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 //document.addEventListener()=>()
 $(document).ready(function(){
-
+    $(".icon").click(function(){
+        if($(".navBar").hasClass("active")){
+            $(".navBar").removeClass("active");
+        }
+        else {console.log("eccomi")
+        $(".navBar").addClass("active");
+        }
+    })
 
 });
