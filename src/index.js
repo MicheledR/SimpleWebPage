144c65b3755482ba15a393924cfb89517e730bdf
Michele de Rosa <michele.derosa@yahoo.com>
2021-11-13 17:51:24 +0100

    inizio meteo

--- src/index.js
+++ src/index.js
@@ -8,5 +8,22 @@ $(document).ready(function(){
         $(".navBar").addClass("active");
         }
     })
+    $('.met').click(function(){
+        console.log("ecco");
+        $.ajax(
+            {
+            //'url':` https://api.openweathermap.org/data/2.5/weather?lat=${latitudine}&long=${longitudine}&appid=0fd24adb757f30dde870736b54dde927`,
+
+            'url':` https://api.openweathermap.org/data/2.5/weather?q=London&appid=0fd24adb757f30dde870736b54dde927`,
+            'method': 'GET',
+            'success': function(risposta){		
+                console.log(risposta);
+            },
+            'error':function(){
+                alert('errore!');
+            }
+            }
+        )
+    });
 
 });
